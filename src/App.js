bd27ca94a65d2d2310480234d0e00ba4b655e92b
Nataset Tanabodee <nataset.t@ku.th>
2022-04-20 21:31:35 +0700

    add axios and test fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [repoData, setRepoData] = useState([]);
+
+    const fetchData = async () => {
+        const response = await axios.get('https://api.github.com/repositories');
+        setRepoData(response.data);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div className="App">
+            <p>{JSON.stringify(repoData)}</p>
+        </div>
+    );
 }
 
 export default App;
